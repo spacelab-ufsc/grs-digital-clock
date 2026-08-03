4f38396511788973467034dc8e806242d66e5791
Anthon Porath Gretter <anthon.png@proton.me>
2026-08-02 21:28:34 -0300

    firmware: enchance of skeleton and changing setup to probe-rs and freezing dependencies

--- firmware/src/bin/main.rs
+++ firmware/src/bin/main.rs
@@ -1,67 +1,62 @@
 #![no_std]
 #![no_main]
+
 #![deny(
     clippy::mem_forget,
-    reason = "mem::forget is generally not safe to do with esp_hal types, especially those \
-    holding buffers for the duration of a data transfer."
+    reason = "mem::forget is generally not safe to do with esp_hal types"
 )]
 #![deny(clippy::large_stack_frames)]
 
-use defmt::info;
-use esp_backtrace as _;
+use defmt::error;
+use embassy_executor::Spawner;
 use esp_hal::clock::CpuClock;
-use esp_hal::main;
-use esp_hal::time::{Duration, Instant};
-use esp_hal::timer::timg::TimerGroup;
-use esp_println as _;
 
-extern crate alloc;
 
-// This creates a default app-descriptor required by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
+#[panic_handler]
+fn panic(info: &core::panic::PanicInfo) -> ! {
+    error!("{}", info);
+    // TODO possibly blink all 7seg ?
+    loop {}
+}
+
+
+// Required by ESP-IDF bootloader format
 esp_bootloader_esp_idf::esp_app_desc!();
 
-#[allow(
-    clippy::large_stack_frames,
-    reason = "it's not unusual to allocate larger buffers etc. in main"
-)]
-#[main]
-fn main() -> ! {
-    // generator version: 1.3.0
-    // generator parameters: --chip esp32c3 -o esp32c3-mini-1 -o unstable-hal -o alloc -o wifi -o ci -o zed -o defmt -o esp-backtrace
 
-    let config = esp_hal::Config::default().with_cpu_clock(CpuClock::max());
+#[esp_rtos::main]
+async fn main(_spawner: Spawner) -> ! {
+    let config = esp_hal::Config::default()
+        .with_cpu_clock(CpuClock::max());
+
     let peripherals = esp_hal::init(config);
 
-    // The following pins are used to bootstrap the chip. They are available
-    // for use, but check the datasheet of the module for more information on them.
-    // - GPIO2
-    // - GPIO8
-    // - GPIO9
-    // These GPIO pins are in use by some feature of the module and should not be used.
-    let _ = peripherals.GPIO11;
-    let _ = peripherals.GPIO12;
-    let _ = peripherals.GPIO13;
-    let _ = peripherals.GPIO14;
-    let _ = peripherals.GPIO15;
-    let _ = peripherals.GPIO16;
-    let _ = peripherals.GPIO17;
+    // Initialize defmt output
+    rtt_target::rtt_init_defmt!();
 
-    esp_alloc::heap_allocator!(#[esp_hal::ram(reclaimed)] size: 66320);
+    // TODO beautiful splash text
+    defmt::info!("ESP32-C3 firmware starting");
 
-    let timg0 = TimerGroup::new(peripherals.TIMG0);
-    let sw_interrupt =
-        esp_hal::interrupt::software::SoftwareInterruptControl::new(peripherals.SW_INTERRUPT);
-    esp_rtos::start(timg0.timer0, sw_interrupt.software_interrupt0);
-    let (mut _wifi_controller, _interfaces) =
-        esp_radio::wifi::new(peripherals.WIFI, Default::default())
-            .expect("Failed to initialize Wi-Fi controller");
+
+    //
+    // Hardware initialization will happen here later:
+    //
+    // - I2C bus
+    //      - AHT25
+    //      - DS3231
+    //
+    // - SPI bus
+    //      - MAX7219
+    //
+    // - Wi-Fi
+    //      - initialized only when required
+    //
+
+
+    let _ = peripherals;
+
 
     loop {
-        info!("Hello world!");
-        let delay_start = Instant::now();
-        while delay_start.elapsed() < Duration::from_millis(500) {}
+        embassy_time::Timer::after_secs(60).await;
     }
-
-    // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.1.0/examples
 }
